3e886d623296a8ff95044db40124f556f159790f
Sanchita <protechcomputereducation2016@gmail.com>
2024-10-29 15:52:16 +0530

    f1

---
+++ Flutter-main/my_application/lib/dashboard.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class dashboard extends StatefulWidget{
+  @override
+  State<dashboard> createState() => _dashboardState();
+}
+
+class _dashboardState extends State<dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body:Center(
+        child:(Text("Dashboard ",))
+      )
+    ) ;
+  }
+}
